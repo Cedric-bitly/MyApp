ea30252e2ebb501ae8e3f965c3e505c6777312ee
Cedric <stormyz22a@gmail.com>
2026-03-06 13:58:05 +0300

    react new codes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,33 @@ import Thirdcomponent from './components/Thirdcomponent';
 import Fourthcomponent from './components/Fourthcomponent';
 import Fifthcomponent from './components/Fifthcomponent';
 import Sixthcomponent from './components/Sixthcomponent';
+import Notfound from './components/Notfound';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+
+
+
+
+
+
+
 function App() {
   return (
+   <Router>
     <div className="App">
+
+      <nav>
+        <Link to="/first">First Component</Link>
+        <Link to="/second">Second Component</Link>
+        <Link to="/third">Third Component</Link>
+        <Link to="/fourth">Fourth Component</Link>
+        <Link to="/fifth">Fifth Component</Link>
+        <Link to="/sixth">Sixth Component</Link>
+        <Link to="/help">Help Center</Link>
+        <Link to="/contactus">Contact Us</Link>
+        
+      </nav>
+
       <header className="App-header">
        <h1> Welcome to app react</h1>
        <h2>React is Fun</h2>
@@ -18,15 +42,20 @@ function App() {
 
       </header>
       {/* Below we render/display/show our components */}
-        <Firstcomponent />
-        <Secondcomponent />
-        <ContactUs/>
-        <HelpCenter/>
-        <Thirdcomponent/>
-        <Fourthcomponent/>
-        <Fifthcomponent/>
-        <Sixthcomponent/>
+      <Routes>
+        <Route path="/help" element={<HelpCenter />}/>
+        <Route path="/first" element={<Firstcomponent />} />
+        <Route path="/second" element={<Secondcomponent />} />
+        <Route path="/third" element={<Thirdcomponent />} />
+        <Route path="/contactUs" element={<ContactUs />}/>
+        <Route path="/fourth" element={<Fourthcomponent />}/>
+        <Route path="/fifth" element={<Fifthcomponent />}/>
+        <Route path="/sixth" element={<Sixthcomponent />}/>
+        <Route path="*" element={<Notfound />}/>
+      </Routes>
+
     </div>
+   </Router> 
   );
 }
 
---
+++ src/components/Notfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div className='notFound'>
+      <h1>404 - Page Not Found...</h1>
+    </div>
+  )
+}
+
+export default Notfound
